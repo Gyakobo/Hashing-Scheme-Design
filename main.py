ffbf5c998cfe3f7d00da41f92b82df29f330f848
Gyakobo <andygyakobo@gmail.com>
2024-06-23 01:07:11 -0700

    Editted main program

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from random import sample
 # Constants
 num_students    = 12000
 address_space   = 15000
-# address_space   = 15013
 
-# Generate 12,000 unique student IDs(assuming IDs are integers for simplicity)
-student_ids = sample(range(1, num_students+3000), num_students) # ensure unique IDs
+# Generate 12,000 unique student IDs
+student_ids = sample(range(1, num_students), num_students) # ensure unique IDs
 
 def hash_function(id, N):
     # N - address space
@@ -14,8 +13,8 @@ def hash_function(id, N):
 
 # Apply the hash function and store in hash table
 hash_table = [0] * address_space
-collisions = 0
 
+collisions = 0
 for student_id in student_ids:
     hash_value = hash_function(student_id, address_space)
     if hash_table[hash_value] == 0:
